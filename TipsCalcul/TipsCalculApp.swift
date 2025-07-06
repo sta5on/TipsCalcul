30d72ef65007b92b792157b8580b48c7aab8d443
sta5on <stan.voronet@gmail.com>
2025-07-06 15:12:08 +0300

    Initial Commit

---
+++ TipsCalcul/TipsCalculApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipsCalculApp.swift
+//  TipsCalcul
+//
+//  Created by Stanislav on 06.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct TipsCalculApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TipsCalculTests/TipsCalculTests.swift
@@ -0,0 +1,17 @@
+//
+//  TipsCalculTests.swift
+//  TipsCalculTests
+//
+//  Created by Stanislav on 06.07.2025.
+//
+
+import Testing
+@testable import TipsCalcul
+
+struct TipsCalculTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
